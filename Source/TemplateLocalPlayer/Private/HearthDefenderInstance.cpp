c877b062432257cb1fcf05aa2c95a47fcee8c9cc
Timothée Fontan <timothee.fontan@gmail.com>
2023-11-29 10:14:58 +0100

    Début menu controller disconnected

---
+++ Source/TemplateLocalPlayer/Private/HearthDefenderInstance.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HearthDefenderInstance.h"
+#include "Misc/CoreDelegates.h"
+
+void UHearthDefenderInstance::Init() {
+	Super::Init();
+
+	FCoreDelegates::OnControllerConnectionChange/*.AddUObject(this, &UHearthDefenderInstance::ControllerStatusIsChanged)*/;
+}
+
+void UHearthDefenderInstance::ControllerStatusIsChanged(bool Connected, int32 PlatformID, int32 UserId)
+{
+	ControllerStatusChanged.Broadcast(Connected);
+}
