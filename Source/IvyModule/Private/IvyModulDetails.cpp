8cc58b46b14f38bd034ab3b9473846677e5bab73
Ashfurr <39094834+Ashfurr@users.noreply.github.com>
2024-04-29 15:22:42 +0200

    Material Belt update + CPP

--- Source/IvyModule/Private/IvyModulDetails.cpp
+++ Source/IvyModule/Private/IvyModulDetails.cpp
@@ -18,12 +18,43 @@ TSharedRef<IDetailCustomization> FIvyModulDetails::MakeInstance()
 	return MakeShareable(new FIvyModulDetails);
 }
 
+#define LOCTEXT_NAMESPACE "IvyModulDetails"
 void FIvyModulDetails::CustomizeDetails(IDetailLayoutBuilder& DetailBuilder)
 {
+	DetailBuilder.GetObjectsBeingCustomized(ObjectsToEdit);
 	
+	IDetailCategoryBuilder& Category = DetailBuilder.EditCategory("TestCat",LOCTEXT("CatName","Ivy Settings"),ECategoryPriority::Important);
+
+	 Category.AddCustomRow(LOCTEXT("Keyword", "Shuffle"))
+	.NameContent()
+	[
+		SNew(STextBlock)
+		.Text(LOCTEXT("NameText","Reassign Materials"))
+		.Font(IDetailLayoutBuilder::GetDetailFont())
+	]
+	.ValueContent()
+	[
+		SNew(SButton)
+		.Text(LOCTEXT("ButtonText","Shuffle"))
+		.OnClicked_Raw(this, &FIvyModulDetails::EditObjects)
+	];
 }
 
 FReply FIvyModulDetails::EditObjects()
 {
+	for(TWeakObjectPtr<UObject> Object : ObjectsToEdit)
+	{
+		if(!Object.IsValid()) continue;
+
+		AIvyStatic* IS = Cast<AIvyStatic>(Object.Get());
+		if(!IS) continue;
+		
+	}
+
+
+	
 	return FReply::Handled();
 }
+
+
+#undef LOCTEXT_NAMESPACE 
